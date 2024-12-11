124806982953f0a3f352342a6726de4a9cc6a3cc
Ryan Bui <buiswrld@bi.lan>
2024-12-10 20:38:19 -0800

    documentation & references

--- data/render.py
+++ data/render.py
@@ -13,12 +13,31 @@ class DataRenderer:
         plt.rcParams['font.family'] = 'Osaka'
         plt.rcParams['font.size'] = 10
 
+    # Reference: (10)
     def show(self, df: DataFrame) -> None:
+        """
+        Displays the DataFrame in an HTML table format.
+        
+        Parameters:
+        df (DataFrame or Series): The DataFrame or Series to display.
+        """
+        # Reference: (16)
         if isinstance(df, Series):
             df = df.to_frame().T 
         display(HTML(df.to_html(index=False)))
 
+    # Reference: (3)
     def split_show(self, df1, df2, title1, title2) -> None:
+        """
+        Renders two DataFrames side by side in HTML table format.
+        args:
+            df1 (DataFrame): First dataframe to display on the left side
+            df2 (DataFrame): Second dataframe to display on the right side
+            title1: A title for the first DataFrame
+            title2: A title for the second DataFrame
+        return:
+            None
+        """
         jsx = f"""
         <div style="display: flex; justify-content: space-around;">
             <div>
@@ -31,10 +50,23 @@ class DataRenderer:
             </div>
         </div>
         """
+        # Reference: (10)
         display(HTML(jsx))
 
+    # Reference: (11)
     def plot(self, df: DataFrame, x_col: str, y_col: str, reverse=False) -> None:
-
+        """
+        Plots a scatter plot of two columns from a given DataFrame.
+        
+        Parameters:
+        df (DataFrame): The DataFrame containing the data.
+        x_col (str): The name of the column to use for the x-axis.
+        y_col (str): The name of the column to use for the y-axis.
+        reverse (bool): Whether to reverse the y-axis. Useful for rank data (1 > 100).
+        
+        Returns:
+        None
+        """
         # Setup
         plt.figure(figsize=(8, 4))
         plt.scatter(df[x_col], df[y_col], alpha=0.6)
@@ -51,8 +83,18 @@ class DataRenderer:
         # Show
         plt.show()
 
+    # Reference: (11)
     def plot_bar(self, df: DataFrame, column: str) -> None:
-
+        """
+        Plots a bar graph of the frequency of values in a column.
+        
+        Parameters:
+        df (DataFrame): The DataFrame containing the data.
+        column (str): The name of the column to plot.
+        
+        Returns:
+        None
+        """
         # Preprocess
         value_counts = df[column].value_counts().sort_index()
 
@@ -68,8 +110,20 @@ class DataRenderer:
         # Show
         plt.show()
 
+    # Reference: (11, 12)
     def plot_density(self, df: DataFrame, column: str, title=None, scaled=True) -> None:
-
+        """
+        Plots a density graph of the values in a column.
+        
+        Parameters:
+        df (DataFrame): The DataFrame containing the data.
+        column (str): The name of the column to plot.
+        title (str): The title of the plot. If no title is provided, the column name is used.
+        scaled (bool): Whether to scale the x-axis to a range 0-100.
+        
+        Returns:
+        None
+        """
         # Preprocess
         if not title:
             title = column
@@ -93,8 +147,18 @@ class DataRenderer:
         # Show
         plt.show()
 
+    # Reference: (11)
     def plot_key_frequency(self, data_dict: dict, title: str) -> None:
-
+        """
+        Plot a bar graph of the frequency of keys in a dictionary of DataFrames.
+        
+        Parameters:
+        data_dict (dict): A dictionary where keys are labels and values are DataFrames.
+        title (str): The title of the plot.
+        
+        Returns:
+        None
+        """
         # Preprocess
         key_counts = pd.Series({key: len(df) for key, df in data_dict.items()})
         key_counts = key_counts.sort_index()
@@ -114,9 +178,21 @@ class DataRenderer:
         # Show
         plt.show()
 
+    # Reference: (11)
     def plot_year_frequency(self, df: DataFrame, date_col: str, year_range: range) -> None:
-
+        """
+        Plot a bar graph of the frequency of years that songs were released.
+        
+        Parameters:
+        df (DataFrame): The DataFrame containing the data.
+        date_col (str): The name of the column containing the dates. Can accept both raw year (e.g. 2021) or full date, if year is the last 4 characters (e.g. 01/01/2021, Jan 1 2025).
+        year_range (range): The range of years to include in the plot.
+        
+        Returns:
+        None
+        """
         # Preprocess
+        # Reference: (13)
         if pd.api.types.is_string_dtype(df[date_col]):
             df['year'] = df[date_col].str[-4:].astype(int)
         
@@ -139,6 +215,7 @@ class DataRenderer:
         # Format
         plt.xticks(rotation=45)
 
+        # Reference: (14, 15)
         if len(year_range) > 50:
             tick_positions = np.linspace(0, len(year_range) - 1, num=20, dtype=int)
             tick_labels = [year_range[i] for i in tick_positions]
